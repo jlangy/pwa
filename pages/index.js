f918d5107d480c5d1202f3fab3c8fefeebeb9e06
Jonathan Langlois <jon@button.is>
2021-11-07 15:44:59 -0800

    swiper

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,39 @@
 import styles from '../styles/Home.module.css'
 import React from 'react';
 import useGeolocation from 'hooks/geolocation';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+
+import 'swiper/css';
 
 export default function Home() {
   const geolocation = useGeolocation();
 
   return (
     <div className={styles.container}>
+      <h1>HIHIHI</h1>
       <main>
-        {`You are at ${geolocation.latitude}, ${geolocation.longitude}`}
+        <p>
+         {`You are at ${geolocation.latitude}, ${geolocation.longitude}`}
+        </p>
+
+        <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        onSwiper={(swiper) => (window.swiper = swiper)}
+        slidesPerView={3}
+        spaceBetween={50}
+        navigation
+        loop
+        scrollbar={{ draggable: true }}
+        pagination={{ clickable: true }}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+      </Swiper>
       </main>
     </div>
   )
