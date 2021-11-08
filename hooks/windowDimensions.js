2551a1c78f0022a3ec2038d46c3a0b382da20e9e
Jonathan Langlois <jon@button.is>
2021-11-07 16:49:28 -0800

    sizing

--- hooks/windowDimensions.js
+++ hooks/windowDimensions.js
@@ -1,25 +1,30 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 function getWindowDimensions() {
-  if (typeof window !== "undefined") {
-    const { innerWidth: width, innerHeight: height } = window;
-    return {
-      width,
-      height
-    };
-  } return {height: 0, width: 0}
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
 }
 
 export default function useWindowDimensions() {
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+  const [windowDimensions, setWindowDimensions] = useState(
+    {
+      height: undefined,
+      width: undefined,
+    }
+  );
 
   useEffect(() => {
-    function handleResize() {
+    if (typeof window !== "undefined") {
       setWindowDimensions(getWindowDimensions());
+      function handleResize() {
+        setWindowDimensions(getWindowDimensions());
+      }
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
     }
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
   }, []);
 
   return windowDimensions;
